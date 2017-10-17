455d67bd5f90f57b132995170a85b3a4f521682d
John M. Wargo <30843404+jwargo@users.noreply.github.com>
2017-10-17 13:53:25 -0400

    Added content and button to the RN app

--- AcmeApp/App.js
+++ AcmeApp/App.js
@@ -1,46 +1,69 @@
 import React, { Component } from 'react';
-import {  Platform, StyleSheet, Text, View } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {  Button, Platform, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>      
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+      <View> 
+        <View style={styles.toolbar}>
+          <Text style={styles.toolbarButton}>Add</Text>
+          <Text style={styles.toolbarTitle}>ReactiveConf 2017</Text>
+          <Text style={styles.toolbarButton}>Like</Text>
+        </View>             
+        <Text style={styles.content}>
+          Kielbasa pork loin turducken shank venison. Shoulder meatloaf porchetta short ribs tongue, ribeye ball tip. Cupim pork loin t-bone doner pork belly tongue ball tip kielbasa pancetta shankle filet mignon jowl landjaeger strip steak. Chicken ham hock turducken, venison spare ribs turkey ribeye boudin pastrami ball tip flank.
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
+        <Text style={styles.content}>
+          Pig short loin burgdoggen bresaola ham ground round. Turkey ball tip prosciutto ground round swine, salami shoulder leberkas. Turkey burgdoggen rump pork. Ham hock flank shankle, jowl andouille burgdoggen leberkas alcatra meatball frankfurter pork belly beef. Pastrami cow picanha tenderloin salami turducken short loin pork chop.
         </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Text style={styles.content}>
+          Content generated using the Bacom Ipsum Generator at https://baconipsum.com/.
+        </Text>      
+        <Button onPress={this.onButtonPress}
+          title="Press me"
+          color="#841584"
+          accessibilityLabel="Purple button"
+        />
       </View>
     )
+  }
+
+  onButtonPress(event){
+    console.log('Button pressed!');
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  // container: {
+  //   flex: 1,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   backgroundColor: '#F5FCFF',
+  // },
+
+  toolbar:{
+    backgroundColor:'blue',
+    paddingTop:30,
+    paddingBottom:10,
+    flexDirection:'row'    
+},
+toolbarButton:{
+    width: 50,
+    color:'#fff',
+    textAlign:'center'
+},
+toolbarTitle:{
+    color:'#fff',
+    textAlign:'center',
+    fontWeight:'bold',
+    flex:1 
+},
+
+  content: {
+    textAlign: 'left',
+    color: 'black',
+    // marginTop: 10,
+    // marginBottom: 10,
+    padding: 10
   },
 });
