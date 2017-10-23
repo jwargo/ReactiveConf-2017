80cfc497c3ac70caf6dbf5b286847993b507e26b
John M. Wargo <30843404+jwargo@users.noreply.github.com>
2017-10-23 11:07:50 -0400

    Updated Mon Oct 23 11:07:50 EDT 2017

--- node_modules/mobile-center-link-scripts/src/index.js
+++
@@ -1,9 +0,0 @@
-const android = require('./android');
-const ios = require('./ios');
-const inquirer = require('inquirer');
-
-module.exports = {
-    android,
-    ios,
-    inquirer
-};
--- node_modules/mobile-center-push/ios/RNPush/RNPush.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface RNPush : NSObject
-
-+ (void)register;
-
-@end
